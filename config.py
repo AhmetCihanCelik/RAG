b918a1e7f5932fbda1e72a2decc547d318029823
Ahmet Cihan ÇELİK <114668396+AhmetCihanCelik@users.noreply.github.com>
2025-07-29 15:40:09 +0300

    küçük düzeltmeler

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
-TRACK_KEYWORD = "Adalet Bakanlığı"
